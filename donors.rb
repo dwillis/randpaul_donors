6fee6bd40896158876685fc74fea75495883a3da
Derek Willis <derek.willis@propublica.org>
2015-09-24 12:32:19 -0400

    initial commit

---
+++ donors.rb
@@ -0,0 +1,36 @@
+require 'rubygems'
+require 'httparty'
+require 'csv'
+require 'date'
+
+loop do
+  date = Date.today
+  r = HTTParty.get("https://secure.randpaul.com/ticker/ticker.php")
+  summary_file = "csv/#{date.to_s.gsub('-','')}_summary.csv"
+  donor_file = "csv/#{date.to_s.gsub('-','')}_donors.csv"
+  if r.parsed_response
+    begin
+      f = File.open(summary_file)
+      sh = f.size > 50 ? false : true
+      f.close
+    rescue
+      sh = true
+    end
+    CSV.open(summary_file, "a+", :write_headers => sh, :headers => ['date','datetime','total_donors','total_amount','start']) do |row|
+      row << [date.to_s, r.response['date'], r.parsed_response['total_donors'], r.parsed_response['total_amount'], r.parsed_response['start']]
+    end
+    begin
+      f = File.open(donor_file)
+      dh = f.size > 50 ? false : true
+      f.close
+    rescue
+      dh = true
+    end
+    CSV.open(donor_file, "a+", :write_headers => dh, :headers => ['date','datetime','first_name','last_name','address']) do |row|
+      r.parsed_response['recent_donors'].each do |recent_donor|
+        row << [date.to_s, r.response['date'], recent_donor['first_name'], recent_donor['last_name'], recent_donor['address']]
+      end
+    end
+  end
+  sleep(60)
+end
